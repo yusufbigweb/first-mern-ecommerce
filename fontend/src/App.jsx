cd71ba547809f9221c8138b551ace8eeb305640b
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-10-17 23:44:24 +0000

    Update

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Routers, Route} from 'react-router-dom'
+import Home from './pagas/home'
+import Collection from './pagas/Collection'
+import About from './pagas/About'
+import Contact from './pagas/Contact'
+import Product from './pagas/Product'
+import Cart from './pagas/Cart'
 
 export const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+
+<Routers>
+<Route path='/' element={<Home/>}/>
+<Route path='./Collection' element={<Collection/>} />
+<Route path='./about' element={<About/>}/>
+<Route path='./contact' element={<Contact/>} />
+<Route path='./product/:productId' element={<Product/>} />
+<Route path='./cart' element={<Cart/>}/>
+
+</Routers>
+
     </div>
   )
 }
