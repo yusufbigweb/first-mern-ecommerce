a5e85b6b2f9ed469e9f31fba3afd5f1272c2a46b
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-10-18 01:08:44 +0000

    Update

--- fontend/src/components/Navbar.jsx
+++ fontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {assets} from '../assets/assets'
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-        <img src={assets.logo} className='w-36' alt="" />
+        <img src={assets.logo} className='w-[36px]' alt="" />
         
     </div>
   )
